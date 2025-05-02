fddd8567273c37d18bfa84f2a27e4b3063896a63
tusharshukla34 <tusharshukla880@gmail.com>
2025-05-02 19:14:56 +0530

    new

--- second.cpp
+++ second.cpp
@@ -13,19 +13,106 @@ int main()
     // cin>>cost;
     // cout<<"Profit you got is :";
     // cout<<sell-cost;
-  int a=5;
-  int b=11;
-  int c=81;
-  int d=61;
-  int e=2;
+  // int a=5;
+  // int b=11;
+  // int c=81;
+  // int d=61;
+  // int e=2;
 
-  cout<<a-- + d-- - c++ - e-- + --c - d++;
+  // cout<<a-- + d-- - c++ - e-- + --c - d++;
 
 //   cout<<sizeof(int);                         ye size nikalne ke liye 
 //   cout<<numeric_limits<double>::min();       ye limit nikalne ke liye
 // cout<<&a;                                    ye address nikalne ke liye
 
 
-    
+
+// Practise⬇️
+
+// -------------------           1           -----------------------------------------
+
+// int num;
+// cout<<"Enter the number: ";
+// cin>>num;
+// if(num%2==0)
+// {
+//   cout<<"The number is even";
+// } else{
+//   cout<<"The number is odd";
+// }
+  
+// -------------------           2            -----------------------------------------
+
+// 2) Write a program that takes a temperature (in Celsius) as input and suggests whether to wear a jacket, shorts, or nothing based on the following conditions:
+
+// Less than 10°C: "Wear a jacket"
+// Between 10°C to 20°C: "Wear a light jacket"
+// Between 21°C to 30°C: "Wear shorts"
+// Above 30°C: "Stay indoors"
+
+// int temp;
+// cout<<"Enter the temperature in celcius: ";
+// cin>>temp;
+// if(temp<=10)
+// {
+//   cout<<"Wear a Jacket";
+// }
+// else if(temp>10 && temp<20)
+// {
+//  cout<<"Wear a light jacket";
+// }
+// else if(temp>21 && temp<30)
+// {
+//   cout<<"Wear shorts";
+// }
+// else if(temp>=30 && temp<=50)
+// {
+//   cout<<"Stay indoors";
+// }
+// else
+// {
+//   cout<<"Ghar mein beth or cooler ki hava kah";
+// }
+
+
+// -------------------           3            -----------------------------------------
+
+// 3)Write a program that takes a year as input and determines if the year belongs to the past, present, or future:
+
+// If the year is in the past (before the current year), print "Past".
+// If the year is in the present (current year), print "Present".
+// If the year is in the future (after the current year), print "Future".
+
+
+int year;
+cout<<"Enter the year: ";
+cin>>year;
+if(year<2025)
+{
+  cout<<"Past";
+}
+else if(year==2025)
+{
+  cout<<"Present year";
+}
+else
+{
+  cout<<"Future";
+}
+
+
+// -------------------           4            -----------------------------------------
+
+// Write a program that takes an integer input and checks if it is:
+
+// Less than 10: "Less than 10"
+// Between 10 and 20 (inclusive): "Between 10 and 20"
+// Between 21 and 30 (inclusive): "Between 21 and 30"
+// Greater than 30: "Greater than 30"
+
+
+
+
+
 
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+int num;
+// cout<<"Enter the number: ";
+// cin>>num;
+// if(num%2==0)
+// {
+//   cout<<"The number is even";
+// } else{
+//   cout<<"The number is odd";
+// }
