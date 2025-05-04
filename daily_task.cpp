57f337b2a5ae66aa09340fa7956c2f78fbf39d0d
tusharshukla34 <tusharshukla880@gmail.com>
2025-05-04 17:44:58 +0530

    new task

---
+++ daily_task.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+// ---------------------1--------------------------------------------------------
+
+int num;
+cout<<"Enter the number";
+cin>>num;
+if(num%3==0)
+{
+    cout<<"No. is divisible";
+}
+else{
+    cout<<"No. is not divisible";
+}
+
+
+// ---------------------2--------------------------------------------------------
+
+
+
+
+
+
+
+
+}
