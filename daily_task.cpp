81a3a75adda68a8c32e8b68b4fcf20bca75ba833
tusharshukla34 <tusharshukla880@gmail.com>
2025-05-04 18:08:16 +0530

    new

--- daily_task.cpp
+++ daily_task.cpp
@@ -5,21 +5,48 @@ int main()
     
 // ---------------------1--------------------------------------------------------
 
-int num;
-cout<<"Enter the number";
-cin>>num;
-if(num%3==0)
-{
-    cout<<"No. is divisible";
-}
-else{
-    cout<<"No. is not divisible";
-}
+// Write a C++ code snippet that takes an integer as input and prints "Divisible by 3" if the number is perfectly divisible by 3, and "Not divisible by 3" otherwise.
+
+// int num;
+// cout<<"Enter the number";
+// cin>>num;
+// if(num%3==0)
+// {
+//     cout<<"No. is divisible";
+// }
+// else{
+//     cout<<"No. is not divisible";
+// }
 
 
 // ---------------------2--------------------------------------------------------
 
+// Write a C++ code snippet that takes two integers as input and prints the larger of the two numbers. If the numbers are equal, it should print "The numbers are equal".
 
+
+
+// int a;
+// int b;
+// cout<<"Enter the first number ";
+// cin>>a;
+// cout<<"Enter the Second number ";
+// cin>>b;
+// if(a>b)
+// {
+//     cout<< a <<" is the larger number";
+// }
+// else if(b>a)
+// {
+//     cout<< b <<" is the larger number";
+// }
+// else
+// {
+//     cout<< a<< " and " << b << " are equal";
+// }
+
+// ---------------------3--------------------------------------------------------
+
+// Write a C++ code snippet that takes an integer as input representing a student's score. If the score is 60 or greater, print "Pass". Otherwise, print "Fail".
 
 
 
